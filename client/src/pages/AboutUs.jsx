ef5f4834f68777a835a8deca4498204b52f1fcbd
Ernest <zyperis14@gmail.com>
2025-09-15 11:49:08 +0300

    aboutus

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -5,24 +5,22 @@ function AboutUS() {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-6 pt-3">
-                    <small className="text-uppercase" style="color: #9B5DE5;">Welcome to Our Journey</small>
-                    <h1 className="h2 mb-4" style="font-weight: 600;">About Our <span style="color: #9B5DE5;">Company</span>
-                    </h1>
-                    <p className="text-secondary" style="line-height: 2;">Embark on a journey with us as we strive to
-                        redefine excellence and innovation in every step. Lorem ipsum dolor sit amet consectetur
-                        adipisicing elit. Suscipit repellat iure laboriosam cum voluptatum, nam minima deserunt aut?
-                        Distinctio voluptatibus dolor quaerat quo omnis illo sequi at velit, odit quod!</p>
-                </div>
-                <div className="col-md-6 text-center">
+                <div className="col-md-6 col-m-3">
                     <div className="row">
-                        <div className="col-md-6 mb-3">
-                            <img src={image1} className="w-100 rounded" alt="" />
+                        <div className="col-md-9 mb-3">
+                            <img src={image1} className="rounded w-100" alt="image1"/>
                         </div>
-                        <div className="col-md-6 mb-3">
-                            <img src={image2} className="w-100 rounded" alt="" />
+                        <div className="col-md-9">
+                            <img src={image2} className="rounded w-100" alt="image2"/>
                         </div>
                     </div>
+                </div>
+                <div className="col-md-6 pt-3">
+                    <small className="text-uppercase">Sveiki atvykę į mūsų kelionę</small>
+                    <h1 className="h1 mb-4">Apie mūsų kirpyklą</h1>
+                    <p className="text-secondary">Leiskitės į kelionę su mumis, nes kiekviename žingsnyje siekiame iš naujo apibrėžti meistriškumą ir naujoves. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit repellat iure laboriosam cum voluptatum, nam minimuma deserunt aut? Distinctio voluptatibus dolor quaerat quo omnis illo sequi at velit, odit quod! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo eligendi vel animi non provident sit illo quaerat voluptatum ratione sunt cumque pariatur maxime, assumenda est consequatur ipsum quam mollitia fugiat delectus architecto quod quis culpa omnis. Excepturi ullam iure est tempora. Nesciunt eos nihil, eveniet, nemo temporibus fugit voluptatum libero repellat excepturi id veritatis sapiente distinctio nulla neque. Culpa pariatur laborum dolores quae alias, quis eos odit iusto voluptate hic accusamus, illo dolorum odio perferendis omnis fuga. In doloribus, doloremque amet laborum unde quibusdam deleniti magni esse eligendi nemo exercitationem vero consequuntur dolore iusto iure porro, accusantium, fuga adipisci velit!
+                        doloremque amet laborum unde quibusdam deleniti magni esse eligendi nemo exercitationem vero consequuntur dolore iustoiure porro, accusantium, fuga adipisci velitdoloremque amet laborum unde quibusdam deleniti magni esse eligendi nemoexercitationem vero consequuntur dolore iusto iure porro, accusantium, fuga adipisci velit
+                    </p>
                 </div>
             </div>
         </div>
