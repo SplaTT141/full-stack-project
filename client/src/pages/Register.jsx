20b1dd15bbf7b7b2b969604576a633abfc9eb21e
Ernest <zyperis14@gmail.com>
2025-09-16 15:27:14 +0300

    registracijos duomenu siuntimas i db

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,25 +1,49 @@
+import { useState } from "react";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 export function Register() {
+
+    const [userData, setUserData] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        axios.post('http://localhost:5000/user_register', userData)
+        .then((res) => {
+            navigate('/')
+            console.log(res.data);
+        })
+        .catch((err) => {console.log(err)})
+    };
+
     return (
     <div className="container">
         <div className="row d-flex justify-content-center">
             <div className="d-flex justify-content-center flex-column col-6 col-xl-4 order-2 order-lg-1">
                 <p className="text-center text-body h1 fw-bold mb-5 mt-4">Registracija</p>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="d-flex flex-row align-items-center mb-4">
                         <div className="form-outline flex-fill">
-                            <input type="text" id="username" className="form-control" />
+                            <input onChange={e => setUserData({...userData, username: e.target.value})} type="text" id="username" className="form-control" />
                             <label className="form-label" htmlFor="username">Vartotojo vardas</label>
                         </div>
                     </div>
                     <div className="d-flex flex-row align-items-center mb-4">
                         <div className="form-outline flex-fill">
-                            <input type="email" id="email" className="form-control" />
+                            <input onChange={e => setUserData({...userData, email: e.target.value})} type="email" id="email" className="form-control" />
                             <label className="form-label" htmlFor="email">El. paštas</label>
                         </div>
                     </div>
                     <div className="d-flex flex-row align-items-center mb-4">
                         <div className="form-outline flex-fill">
-                            <input type="password" id="password" className="form-control" />
+                            <input onChange={e => setUserData({...userData, password: e.target.value})} type="password" id="password" className="form-control" />
                             <label className="form-label" htmlFor="password">Slaptažodis</label>
                         </div>
                     </div>
@@ -34,7 +58,7 @@ export function Register() {
                         <label className="form-check-label" htmlFor="checkbox">Sutinku su vartotojo sąlygomis</label>
                     </div>
                     <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <button type="button" className="btn btn-primary btn-lg">Registruotis</button>
+                        <button type="submit" className="btn btn-primary btn-lg">Registruotis</button>
                     </div>
                 </form>
             </div>
--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const mysql = require('mysql');
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+
 
 const app = express();
 const port = 5000;
+
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173",
+}));
+
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -19,6 +27,22 @@ db.connect((error) => {
         console.log('Database is connected!');
     }
 });
+
+app.post('/user_register', (req, res) => {
+    const sql = "INSERT INTO users (`username`, `email`, `password`) VALUES (?, ?, ?) ";
+    const userData = [
+        req.body.username,
+        req.body.email,
+        req.body.password,
+    ]
+    db.query(sql, userData, (error, result) => {
+        if (error) {
+            return res.json({ message: 'Something went wrong' + error })
+        } else {
+            return res.json({ success: 'User added successfully' })
+        }
+    });
+})
 
 app.get('/', (req, res) => {
     res.send("It's alive!");
