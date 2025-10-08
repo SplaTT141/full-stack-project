a160560776274e33244ee86b37f1ceac811f681c
Ernest <zyperis14@gmail.com>
2025-10-08 12:10:13 +0300

    axios remake to fetch

--- client/src/context/user/UserContextWrapper.jsx
+++ client/src/context/user/UserContextWrapper.jsx
@@ -1,13 +1,14 @@
-import { useEffect, useState } from "react"
-import { initialUserContext } from "./initialUserContext"
+// import { useEffect, useState } from "react"
+// import { initialUserContext } from "./initialUserContext"
+// import axios from "axios";
 
-export function UserContextWrapper() {
-    const [isLoggedIn, setIsLoggedIn] = useState(initialUserContext.isLoggedIn);
-    const [role, setRole] = useState(initialUserContext.role);
-    const [email, setEmail] = useState(initialUserContext.email);
-    const [id, setId] = useState(initialUserContext.id);
+// export function UserContextWrapper() {
+//     const [isLoggedIn, setIsLoggedIn] = useState(initialUserContext.isLoggedIn);
+//     const [role, setRole] = useState(initialUserContext.role);
+//     const [email, setEmail] = useState(initialUserContext.email);
+//     const [id, setId] = useState(initialUserContext.id);
 
-    useEffect(() => {
-        
-    }, []);
-}
+//     useEffect(async () => {
+
+//     }, []);
+// }
