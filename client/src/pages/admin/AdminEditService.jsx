57d9a357b9650e2f06c70724e1a45045924528f3
Ernest <zyperis14@gmail.com>
2025-10-31 17:32:09 +0200

    service update final

--- client/src/pages/admin/AdminEditService.jsx
+++ client/src/pages/admin/AdminEditService.jsx
@@ -4,15 +4,20 @@ import { Link, useNavigate, useParams } from "react-router-dom"
 import { useContext } from "react";
 import { UserContext } from "../../context/user/UserContext";
 import { LoginRequired } from "../../components/LoginRequired";
+import { serviceNameIsInvalid, serviceDurationIsInvalid, servicePriceIsInvalid } from "../../lib/validation";
 
 export function AdminEditService() {
     const { isLoggedIn } = useContext(UserContext);
+    const navigate = useNavigate();
 
     const { id } = useParams();
     const [serviceData, setServiceData] = useState('');
+    
+    const [serviceError, setServiceError] = useState('');
+    const [durationError, setDurationError] = useState('');
+    const [priceError, setPriceError] = useState('');
+    
     const [error, setError] = useState('');
-
-    const navigate = useNavigate();
 
     useEffect(() => {
         fetch('http://localhost:5000/services', {
@@ -38,6 +43,29 @@ export function AdminEditService() {
             e.preventDefault();
 
             const {id, service, duration, price} = serviceData;
+
+            setServiceError('');
+            setDurationError('');
+            setPriceError('');
+
+            let hasError = false;
+
+            if (serviceNameIsInvalid(service)) {
+                setServiceError(serviceNameIsInvalid(service));
+                hasError = true;
+            }
+
+            if (serviceDurationIsInvalid(duration)) {
+                setDurationError(serviceDurationIsInvalid(duration));
+                hasError = true;
+            }
+
+            if (servicePriceIsInvalid(price)) {
+                setPriceError(servicePriceIsInvalid(price));
+                hasError = true;
+            }
+
+            if (hasError) return;
 
             fetch(`http://localhost:5000/admin/services/edit`, {
                 method: 'PUT',
@@ -77,10 +105,13 @@ export function AdminEditService() {
                             </div>
                             <label htmlFor="service" className="form-label">Paslaugos pavadinimas</label>
                             <input onChange={(e) => setServiceData({...serviceData, service:e.target.value})} type="text" id="service" className="form-control" value={serviceData.service ?? ''} required />
+                            <div style={{color: 'red'}}>{serviceError}</div>
                             <label htmlFor="duration" className="form-label">Trukmė</label>
                             <input onChange={(e) => setServiceData({...serviceData, duration:+e.target.value})} type="number" id="duration" className="form-control" value={serviceData.duration ?? ''} required />
+                            <div style={{color: 'red'}}>{durationError}</div>
                             <label htmlFor="price" className="form-label">Kaina</label>
                             <input onChange={(e) => setServiceData({...serviceData, price:+e.target.value})} type="number" id="price" className="form-control" value={serviceData.price ?? ''} required />
+                            <div style={{color: 'red'}}>{priceError}</div>
                             <button type="submit" className="btn btn-primary mt-4">Patvirtinti</button>
                             <Link to={'/admin/services'} className="btn btn-danger mt-4 ms-2">Atšaukti</Link>
                         </form>
