5e625af4ec83c99ea37fdca2db159db48b4060bb
Ernest <zyperis14@gmail.com>
2025-11-11 21:28:19 +0200

    WIP

--- client/src/pages/admin/AdminEditReservation.jsx
+++ client/src/pages/admin/AdminEditReservation.jsx
@@ -8,7 +8,7 @@ export function AdminEditReservation() {
     const { id } = useParams(); 
     const { isLoggedIn } = useContext(UserContext);
 
-    const [reservation, setReservation] = useState([]);
+    const [reservationData, setReservationData] = useState([]);
     
     const [error, setError] = useState('');
 
@@ -22,7 +22,7 @@ export function AdminEditReservation() {
                 if (data.status === 'success') {
                     const filtered = data.reservations.find(r => r.id === +id);
                     if (filtered) {
-                        setReservation(filtered);
+                        setReservationData(filtered);
                     } else {
                         setError('Tokia rezervacija nerasta')
                     }
@@ -45,25 +45,24 @@ export function AdminEditReservation() {
                         <div className="mb-3 fw-bold" style={{color: 'red'}}>{error}</div>
                         <form>
                             <div htmlFor="service" className="form-label">ID:
-                                <span className="text-primary"> {reservation.id}</span>
+                                <span className="text-primary"> {reservationData.id}</span>
                             </div>
                             <label htmlFor="name" className="form-label">Vardas</label>
-                            <input type="text" id="name" className="form-control" value={reservation.name ?? ''} required />
+                            <input type="text" onChange={e => setReservationData({...reservationData, name:e.target.value})} id="name" className="form-control" value={reservationData.name ?? ''} required />
                             <label htmlFor="surname" className="form-label">Pavardė</label>
-                            <input type="text" id="surname" className="form-control" value={reservation.surname ?? ''} required />
+                            <input type="text" onChange={e => setReservationData({...reservationData, surname:e.target.value})} id="surname" className="form-control" value={reservationData.surname ?? ''} required />
                             <label htmlFor="email" className="form-label">El. paštas</label>
-                            <input type="email" id="email" className="form-control" value={reservation.email ?? ''} required />
+                            <input type="email" onChange={e => setReservationData({...reservationData, email:e.target.value})} id="email" className="form-control" value={reservationData.email ?? ''} required />
                             <label htmlFor="text" className="form-label">Telefono numeris</label>
-                            <input type="text" id="phone" className="form-control" value={reservation.phone ?? ''} required />
+                            <input type="text" onChange={e => setReservationData({...reservationData, phone:e.target.value})} id="phone" className="form-control" value={reservationData.phone ?? ''} required />
                             <label htmlFor="text" className="form-label">Paslauga</label>
-                            <input type="text" id="service" className="form-control" value={reservation.service_name ?? ''} required />
+                            <input type="text" id="service" className="form-control" value={reservationData.service_name ?? ''} required />
                             <label htmlFor="date" className="form-label">Data</label>
-                            <input type="date" id="date" className="form-control" value={reservation.date?.slice(0, 10) ?? ''} required />
+                            <input type="date" id="date" className="form-control" value={reservationData.date?.slice(0, 10) ?? ''} required />
                             <label htmlFor="time" className="form-label">Laikas</label>
-                            <input type="time" id="time" className="form-control" value={reservation.time ?? ''} required />
+                            <input type="time" id="time" className="form-control" value={reservationData.time ?? ''} required />
                             <button type="submit" className="btn btn-primary mt-4">Patvirtinti</button>
                             <Link to={'/admin/reservations'} className="btn btn-danger mt-4 ms-2">Atšaukti</Link>
-
                         </form>
                     </div>
                 </div>
