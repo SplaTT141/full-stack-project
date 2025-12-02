706ad3e1ce42456d2f7844f2889931ea1b747277
Ernest <zyperis14@gmail.com>
2025-12-02 17:01:52 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(helmet());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://kirpykla.netlify.app",
     credentials: true,
 }));
 app.use(cookieParser);
