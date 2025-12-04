59bd0f7aaba830d60f4d0b2aec6ca68c9d994834
Ernest <zyperis14@gmail.com>
2025-12-04 15:28:10 +0200

    up

--- server/db.js
+++ server/db.js
@@ -3,10 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env.production' });
 
-export const db = await mysql.createPool({
+export const db = mysql.createPool({
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
