03f9fcf634313d9d3e53dc1003bc7680463a9393
Ernest <zyperis14@gmail.com>
2026-01-13 12:56:37 +0200

    charset update

--- server/db.js
+++ server/db.js
@@ -9,4 +9,5 @@ export const db = await mysql.createPool({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
+    charset: "utf8mb4",
 });
